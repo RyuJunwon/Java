ee11554917147e8de63c54be9d6d368e4de8de95
RyuJunwon <137625747+RyuJunwon@users.noreply.github.com>
2023-09-26 21:41:23 +0900

    Add files via upload

---
+++ array/src/arrayTest/ArTest1.java
@@ -0,0 +1,25 @@
+package arrayTest;
+
+public class ArTest1 {
+	public static void main(String[] args) {
+//		int[] arData = {3, 5, 2, 8, 9};
+		int[] arData = new int[5];
+		
+//		System.out.println(arData);
+//		System.out.println(arData.length);
+		
+//		for (int i = 0; i < arData.length; i++) {
+//			System.out.println(arData[i]);
+//		}
+		
+//		5, 4, 3, 2, 1을 arData에 순서대로 넣고 출력
+		
+//		for (int i = 0; i < 5; i++) {
+//			arData[i] = 5 - i;
+//		}
+//		for (int i = 0; i < arData.length; i++) {
+//			System.out.println(arData[i]);
+//		}
+		
+	}
+}
---
+++ array/src/arrayTest/ArrTest.java
@@ -0,0 +1,26 @@
+package arrayTest;
+
+import java.util.Iterator;
+
+public class ArrTest {
+	public static void main(String[] args) {
+		int[][] arrData = {{3, 4, 5}, {6, 7, 8}};
+
+		for (int i = 0; i < arrData.length; i++) {
+			for (int j = 0; j < arrData[i].length; j++) {
+				System.out.println(arrData[i][j]);
+			}
+		}			
+		
+//		int length = 0;
+		
+//		for (int i = 0; i < arrData.length; i++) {
+//			length += arrData[i].length; 
+//		}
+		
+//		for (int i = 0; i < length; i++) {
+//			System.out.println(arrData[i / 3][i % 3]); 
+//		}
+
+	}
+}
