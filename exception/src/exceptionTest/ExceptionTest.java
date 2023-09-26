9e7f251d744a38dd156edc977b900c6f6c4f57c1
RyuJunwon <137625747+RyuJunwon@users.noreply.github.com>
2023-09-26 21:43:19 +0900

    Add files via upload

---
+++ exception/src/exceptionTest/ExceptionTest.java
@@ -0,0 +1,37 @@
+package exceptionTest;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionTest {
+	public static void main(String[] args) {
+		
+		
+		
+		
+//		try {
+//			String data = "류준원";
+//			Integer.parseInt(data);
+//		} catch (NumberFormatException e) {
+//			System.out.println("정수만 입력 가능합니다.");
+//		}
+//		
+		
+		
+//		try {
+//			System.out.println("입력: ");
+//			new Scanner(System.in).nextInt();
+//		} catch (InputMismatchException e) {
+//			System.out.println("정수만 입력하라고 했잖아요 ㅎㅎ");
+//		}
+//		
+		
+//		try {
+//			System.out.println(10 / 0);
+//		} catch (ArithmeticException e) {
+//			e.printStackTrace();	// 현재 시점에서 연계된 모든 코드부분의 라인 번호가 출력된다.
+//			System.out.println(e.getMessage());
+//			System.err.println("0으로 나눌 수 없습니다.");
+//		} 
+	}
+}
