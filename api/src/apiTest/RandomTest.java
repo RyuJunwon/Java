abc58efe555106c38f2f26f1e7a08f8cd13fffa0
RyuJunwon <137625747+RyuJunwon@users.noreply.github.com>
2023-09-26 21:41:08 +0900

    Add files via upload

---
+++ api/src/apiTest/RandomTest.java
@@ -0,0 +1,10 @@
+package apiTest;
+
+import java.util.Random;
+
+public class RandomTest {
+	public static void main(String[] args) {
+		Random random = new Random();
+		System.out.println(random.nextInt(3));
+	}
+}
